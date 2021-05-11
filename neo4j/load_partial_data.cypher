774c2678a6e7bec01345228a496127ddb6b09276
Kacper <gfw.kra@gmail.com>
2021-05-11 02:01:45 +0200

    Added neo4j scripts

---
+++ neo4j/load_partial_data.cypher
@@ -0,0 +1,91 @@
+// CSV Data import Cypher script
+
+// user nodes
+USING PERIODIC COMMIT 10000
+LOAD CSV WITH HEADERS FROM 'file:///' + $fileprefix + '-user.csv' AS row
+FIELDTERMINATOR ';'
+CREATE 
+(author:User{
+    id: row.user_id,
+    is_verified: toBoolean(row.is_verified),
+    account_creation: toInteger(row.account_creation),
+    follower_count: CASE trim(row.follower_count) WHEN "" THEN null ELSE toInteger(row.follower_count) END,
+    following_count: CASE trim(row.following_count) WHEN "" THEN null ELSE toInteger(row.following_count) END
+})
+;
+
+// tweeet nodes
+USING PERIODIC COMMIT 10000
+LOAD CSV WITH HEADERS FROM 'file:///' + $fileprefix + '-tweet.csv' AS row
+FIELDTERMINATOR ';'
+CREATE
+(tweet: Tweet{
+    id: row.tweet_id,
+    text_tokens: split(row.text_tokens, "\t"),
+    hashtags: split(row.hashtags, "\t"),
+    present_media: split(row.present_media, "\t"),
+    present_links: split(row.present_links, "\t"),
+    present_domains: split(row.present_domains, "\t"),
+    tweet_type: row.tweet_type,
+    language: row.language,
+    timestamp: toInteger(row.tweet_timestamp)
+})
+;
+
+// id indexes, for speed
+CREATE CONSTRAINT users_unique IF NOT EXISTS ON (u:User) ASSERT u.id IS UNIQUE;
+CREATE CONSTRAINT tweets_unique IF NOT EXISTS ON (t:Tweet) ASSERT t.id IS UNIQUE;
+
+// author relationship
+// USING PERIODIC COMMIT 5000
+// LOAD CSV WITH HEADERS FROM 'file:///' + $fileprefix + '-relationship.csv' AS row
+// FIELDTERMINATOR ';'
+// MATCH (author :User{ id: row.enaging_user_id })
+// MATCH (tweet: Tweet{ id: row.tweet_id })
+// CREATE (author)-[r:Author]->(tweet)
+// ;
+
+// retweet relationships
+USING PERIODIC COMMIT 5000
+LOAD CSV WITH HEADERS FROM 'file:///' + $fileprefix + '-retweet.csv' AS row
+FIELDTERMINATOR ';'
+MATCH (user: User{ id: row.user_id })
+MATCH (tweet: Tweet{ id: row.tweet_id })
+CREATE (user)-[:Retweet{ timestamp: row.timestamp }]->(tweet)
+;
+
+// like relationships
+USING PERIODIC COMMIT 5000
+LOAD CSV WITH HEADERS FROM 'file:///' + $fileprefix + '-like.csv' AS row
+FIELDTERMINATOR ';'
+MATCH (user: User{ id: row.user_id })
+MATCH (tweet: Tweet{ id: row.tweet_id })
+CREATE (user)-[:Like{ timestamp: row.timestamp }]->(tweet)
+;
+
+// reply relationships
+USING PERIODIC COMMIT 5000
+LOAD CSV WITH HEADERS FROM 'file:///' + $fileprefix + '-reply.csv' AS row
+FIELDTERMINATOR ';'
+MATCH (user: User{ id: row.user_id })
+MATCH (tweet: Tweet{ id: row.tweet_id })
+CREATE (user)-[:Reply{ timestamp: row.timestamp }]->(tweet)
+;
+
+// retweet+comment relationships
+USING PERIODIC COMMIT 5000
+LOAD CSV WITH HEADERS FROM 'file:///' + $fileprefix + '-rtcomment.csv' AS row
+FIELDTERMINATOR ';'
+MATCH (user: User{ id: row.user_id })
+MATCH (tweet: Tweet{ id: row.tweet_id })
+CREATE (user)-[:RetweetComment{ timestamp: row.timestamp }]->(tweet)
+;
+
+// follow relationships
+USING PERIODIC COMMIT 5000
+LOAD CSV WITH HEADERS FROM 'file:///' + $fileprefix + '-follow.csv' AS row
+FIELDTERMINATOR ';'
+MATCH (user1 :User{ id: row.user1_id })
+MATCH (user2 :User{ id: row.user2_id })
+MERGE (user1)-[:Follow]->(user2)
+;
---
+++ neo4j/lzo2csv.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+#
+# Convert LZO file to set of csv, ready for neo4j import
+#
+
+import subprocess
+import csv
+import sys
+import os
+
+from tqdm import tqdm
+
+tweet_features = [
+    "text_tokens", "hashtags", 
+    "tweet_id", "present_media", 
+    "present_links", "present_domains", 
+    "tweet_type", "language", 
+    "tweet_timestamp",
+]
+user_features = [
+    "user_id", "follower_count", "following_count", "is_verified", "account_creation"
+] # X 2 for engaged with and engaging
+relationships_features = [
+    "engagee_follows_engager", "reply_timestamp", 
+    "retweet_timestamp", "retweet_with_comment_timestamp", 
+    "like_timestamp"
+]
+user_tweet_features = [
+    "user_id", "tweet_id", "timestamp"
+]
+user_user_features = [
+    "user1_id", "user2_id"
+]
+
+if len(sys.argv) < 5:
+    print(f"Usage: {sys.argv[0]} input.lzo output user_ids tweet_ids")
+    exit(1)
+
+input_lzo = sys.argv[1]
+output_fp = sys.argv[2]
+user_ids_fp = sys.argv[3]
+tweet_ids_fp = sys.argv[4]
+tweet_data_fp = output_fp + "tweet.csv"
+user_data_fp = output_fp + "user.csv"
+follow_fp = output_fp + "follow.csv"
+reply_fp = output_fp + "reply.csv"
+retweet_fp = output_fp + "retweet.csv"
+like_fp = output_fp + "like.csv"
+comment_fp = output_fp + "rtcomment.csv"
+
+user_ids = set()
+tweet_ids = set()
+if os.path.exists(user_ids_fp):
+    print("Reading existing user ids...")
+    user_ids_f = open(user_ids_fp, "r")
+    for line in user_ids_f.readlines():
+        user_ids.add(line.strip())
+    user_ids_f.close()
+    print(f"Existing ids: {len(user_ids)} user")
+if os.path.exists(tweet_ids_fp):
+    print("Reading existing tweet ids...")
+    tweet_ids_f = open(tweet_ids_fp, "r")
+    for line in tweet_ids_f.readlines():
+        tweet_ids.add(line.strip())
+    tweet_ids_f.close()
+    print(f"Existing ids: {len(tweet_ids)} tweet")
+new_user_ids = set()
+new_tweet_ids = set()
+
+with open(tweet_data_fp, "w") as tweet_f, \
+     open(user_data_fp, "w") as user_f, \
+     open(follow_fp, "w") as follow_f, \
+     open(reply_fp, "w") as reply_f, \
+     open(retweet_fp, "w") as retweet_f, \
+     open(like_fp, "w") as like_f, \
+     open(comment_fp, "w") as comment_f:
+    # csv writer objects
+    tweet_writer = csv.writer(tweet_f, delimiter=";")
+    user_writer = csv.writer(user_f, delimiter=";")
+    follows_writer = csv.writer(follow_f, delimiter=";")
+    replies_writer = csv.writer(reply_f, delimiter=";")
+    retweet_writer = csv.writer(retweet_f, delimiter=";")
+    like_writer = csv.writer(like_f, delimiter=";")
+    comment_writer = csv.writer(comment_f, delimiter=";")
+    # csv headers 
+    tweet_writer.writerow(tweet_features)
+    user_writer.writerow(user_features)
+    follows_writer.writerow(user_user_features)
+    replies_writer.writerow(user_tweet_features)
+    retweet_writer.writerow(user_tweet_features)
+    like_writer.writerow(user_tweet_features)
+    comment_writer.writerow(user_tweet_features)
+
+    print(f"Decompressing {input_lzo}...")
+    proc = subprocess.Popen(
+        ['lzop', '-dc', input_lzo],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT
+    )
+    pbar = tqdm(desc="Reading rows", postfix="rows read/written")
+    for line in iter(proc.stdout.readline, b''):
+        # decode
+        line_utf8 = line.decode('utf-8').strip()
+        cols = line_utf8.split("\x01")
+        # write to csv files
+        pos = 0
+        tweet_id = cols[pos + 2]
+        if tweet_id not in tweet_ids:
+            tweet_writer.writerow(cols[pos:pos+len(tweet_features)])
+            tweet_ids.add(tweet_id)
+            new_tweet_ids.add(tweet_id)
+        pos = pos + len(tweet_features)
+
+        engaged_with_id = cols[pos]
+        if engaged_with_id not in user_ids:
+            user_writer.writerow(cols[pos:pos+len(user_features)])
+            user_ids.add(engaged_with_id)
+            new_user_ids.add(engaged_with_id)
+        pos = pos + len(user_features)
+
+        engaging_id = cols[pos]
+        if engaging_id not in user_ids:
+            user_writer.writerow(cols[pos:pos+len(user_features)])
+            user_ids.add(engaging_id)
+            new_user_ids.add(engaging_id)
+        
+        pos = pos + len(user_features)
+        follows, reply, retweet, comment, like = cols[pos:pos+len(relationships_features)]
+        if follows == 'true':
+            follows_writer.writerow([engaged_with_id, engaging_id])
+        if len(reply) != 0:
+            replies_writer.writerow([engaged_with_id, tweet_id, reply])
+        if len(retweet) != 0:
+            retweet_writer.writerow([engaged_with_id, tweet_id, retweet])
+        if len(comment) != 0:
+            comment_writer.writerow([engaged_with_id, tweet_id, comment])
+        if len(like) != 0:
+            like_writer.writerow([engaged_with_id, tweet_id, like])
+        # update progress
+        pbar.update(1)
+    pbar.close()
+    proc.kill()
+print(f"Writing new ids: {len(new_user_ids)} new user, {len(new_tweet_ids)} new tweet")
+with open(user_ids_fp, "a") as user_ids_f, open(tweet_ids_fp, "a") as tweet_ids_f:
+    for uid in new_user_ids:
+        user_ids_f.write(f"{uid}\n")
+    for tid in new_tweet_ids:
+        tweet_ids_f.write(f"{tid}\n")
+print("Done!")
+        
