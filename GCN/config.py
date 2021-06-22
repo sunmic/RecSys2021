5a95f8b71a8f0c80f1e89cdcd7cc768575edeca2
Kacper Rączy <gfw.kra@gmail.com>
2021-06-22 12:20:31 +0200

    Bumb torch version

--- GCN/config.py
+++ GCN/config.py
@@ -7,14 +7,14 @@ import torch_geometric
 hostname = socket.gethostname()
 
 # required by colab worker
-# torch-cluster @ https://pytorch-geometric.com/whl/torch-1.8.0+cu102.html
-# torch-scatter @ https://pytorch-geometric.com/whl/torch-1.8.0+cu102.html
-# torch-sparse @ https://pytorch-geometric.com/whl/torch-1.8.0+cu102.html
-# torch-spline-conv @ https://pytorch-geometric.com/whl/torch-1.8.0+cu102.html
-Task.add_requirements('torch-scatter', '@ https://pytorch-geometric.com/whl/torch-1.8.0+cu102.html')
-Task.add_requirements('torch-sparse', '@ https://pytorch-geometric.com/whl/torch-1.8.0+cu102.html')
-Task.add_requirements('torch-cluster', '@ https://pytorch-geometric.com/whl/torch-1.8.0+cu102.html')
-Task.add_requirements('torch-spline-conv', '@ https://pytorch-geometric.com/whl/torch-1.8.0+cu102.html')
+# torch-cluster @ https://pytorch-geometric.com/whl/torch-1.9.0+cu102.html
+# torch-scatter @ https://pytorch-geometric.com/whl/torch-1.9.0+cu102.html
+# torch-sparse @ https://pytorch-geometric.com/whl/torch-1.9.0+cu102.html
+# torch-spline-conv @ https://pytorch-geometric.com/whl/torch-1.9.0+cu102.html
+Task.add_requirements('torch-scatter', '@ https://pytorch-geometric.com/whl/torch-1.9.0+cu102.html')
+Task.add_requirements('torch-sparse', '@ https://pytorch-geometric.com/whl/torch-1.9.0+cu102.html')
+Task.add_requirements('torch-cluster', '@ https://pytorch-geometric.com/whl/torch-1.9.0+cu102.html')
+Task.add_requirements('torch-spline-conv', '@ https://pytorch-geometric.com/whl/torch-1.9.0+cu102.html')
 Task.add_requirements('neo4j')
 Task.add_requirements('transformers')
 Task.add_requirements('torchmetrics')
